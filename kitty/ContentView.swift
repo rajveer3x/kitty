3e0fb4cba1be0a27ef613b731855354e75603a69
rajveer3x <panwarrajveer07@gmail.com>
2026-09-06 00:20:34 +0530

    Initial Commit

---
+++ kitty/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  kitty
+//
+//  Created by Rajveer Singh on 06/09/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ kitty/kittyApp.swift
@@ -0,0 +1,17 @@
+//
+//  kittyApp.swift
+//  kitty
+//
+//  Created by Rajveer Singh on 06/09/26.
+//
+
+import SwiftUI
+
+@main
+struct kittyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
